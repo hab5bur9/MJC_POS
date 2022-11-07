7b93f9c61e904c277703ffe30fc104643856110c
hsmik <hsmik@172.111.117.93>
2022-11-07 21:37:05 +0900

    KB Git connect

--- MJC_POS/src/pos/HelloWorld.java
+++ MJC_POS/src/pos/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("하이용");
-
+		System.out.println("KB Git connect");
+		System.out.println("KB Git connect");
+		System.out.println("KB Git connect");
+		
+				
 	}
 
 }
