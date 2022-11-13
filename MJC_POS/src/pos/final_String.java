70cfea462f13bd675886621010cc1c2540da2b0c
user <user@DESKTOP-T4OLPC3>
2022-11-14 00:56:59 +0900

    change

--- MJC_POS/src/pos/final_String.java
+++ MJC_POS/src/pos/final_String.java
@@ -5,39 +5,39 @@ public final class final_String {
 	
 	
 	//  header
-	public static final String HEADER_BACKMENU="�룎�븘媛�湲�";
+	public static final String HEADER_BACKMENU="돌아가기";
 	
 	// order page start
 	
-	public static final String ORDERMENU_TITLE ="諛쒖＜"; 
-	public static final String ORDERMENU_ORDERR_TAP = "諛쒖＜";
-	public static final String ORDERMENU_RECOMMEND_TAP = "異붿쿇";
-	public static final String ORDERMENU_CONFRIM = "諛쒖＜";
-	public static final String ORDERMENU_ADD ="異붽�";
-	public static final String ORDERMENU_DELETE="�궘�젣";
-	public static final String PLZ_COOSE_PRODUCT="�긽�뭹�쓣 �꽑�깮 �빐 二쇱꽭�슂"; 
-	public static final String TOTAL_PRICE = "珥� 湲덉븸";
-	
+	public static final String ORDERMENU_TITLE ="발주"; 
+    public static final String ORDERMENU_ORDERR_TAP = "발주";
+    public static final String ORDERMENU_RECOMMEND_TAP = "추천";
+    public static final String ORDERMENU_CONFRIM = "발주";
+    public static final String ORDERMENU_ADD ="추가";
+    public static final String ORDERMENU_DELETE="삭제";
+    public static final String PLZ_COOSE_PRODUCT="상품을 선택 해 주세요"; 
+    public static final String TOTAL_PRICE = "총 금액";
+
 	
 	//Login page
-	public static final String LOGIN_LOGIN = "   濡쒓렇�씤    ";
-	public static final String LOGIN_CANCLE = "     痍⑥냼     ";
-	public static final String LOGIN_RESET = "          鍮꾨�踰덊샇  珥덇린�솕           ";
-	public static final String LOGIN_ID ="ID �엯�젰 : ";
-	public static final String LOGIN_PW ="PW �엯�젰 :";
+	public static final String LOGIN_LOGIN = "   로그인    ";
+	public static final String LOGIN_CANCLE = "     취소     "	;
+	public static final String LOGIN_RESET = "          비밀번호  초기화           ";
+	public static final String LOGIN_ID ="ID 입력 : ";
+	public static final String LOGIN_PW ="PW 입력 :";
 	
 	//Sell page
-	public static final String SELL_BTN_CASH ="�쁽湲�";
-	public static final String SELL_BTN_CARD ="�떊�슜移대뱶";
-	public static final String SELL_BTN_TRAFFIC_CARD ="援먰넻移대뱶";
-	public static final String SELL_BTN_MOBILE ="紐⑤컮�씪�럹�씠";
-	public static final String SELL_TOTAL_PRICE ="珥� 湲덉븸 :";
+	public static final String SELL_BTN_CASH ="현금";
+	public static final String SELL_BTN_CARD ="신용카드";
+	public static final String SELL_BTN_TRAFFIC_CARD ="교통카드";
+	public static final String SELL_BTN_MOBILE ="모바일페이";
+	public static final String SELL_TOTAL_PRICE ="총 금액 : ";
 	
 	//bil_select page
-	public static final String BILL_SELECT_DEAL_PRODUCT_LABEL ="嫄곕옒 �긽�뭹 寃��깋";
-	public static final String BILL_SELECT_DEAL_BEAKDOWN_LABEL ="嫄곕옒 �궡�뿭 寃��깋";
-	public static final String BILL_SELECT_BILL_OUTPUT ="�쁺�닔利� 異쒕젰";
-	public static final String BILL_SELECT_REFUND ="�솚遺�";
+	public static final String BILL_SELECT_DEAL_PRODUCT_LABEL ="거래 상품 검색";
+	public static final String BILL_SELECT_DEAL_BEAKDOWN_LABEL ="거래 내역 검색";
+	public static final String BILL_SELECT_BILL_OUTPUT ="영수증 출력";
+	public static final String BILL_SELECT_REFUND ="환불";
 
 	
 	
@@ -45,3 +45,4 @@ public final class final_String {
 	
 	
 }
+	
