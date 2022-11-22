36363e1772a0ed0112be2a4372f2abf81aac123d
ca080 <ca080@172.16.11.206>
2022-11-23 01:04:09 +0900

    .

--- MJC_POS/src/pos/Add_Staff_Frame.java
+++ MJC_POS/src/pos/Add_Staff_Frame.java
@@ -15,6 +15,7 @@ public class Add_Staff_Frame extends JFrame{
 	
 	JButton btn_backMain=new JButton("돌아가기");
 	JButton btn_save=new JButton("저장");
+	JButton btn_empty=new JButton("다시 쓰기");
 	
 	JTextField name_txt=new JTextField();
 	JTextField age_txt=new JTextField();
@@ -33,6 +34,7 @@ public class Add_Staff_Frame extends JFrame{
 		text_panel.setBackground(Color.LIGHT_GRAY);
 		main_panel.add(text_panel);
 		main_panel.add(btn_backMain);
+		main_panel.add(btn_empty);
 		
 		text_panel.add(name);
 		text_panel.add(age);
@@ -42,7 +44,10 @@ public class Add_Staff_Frame extends JFrame{
 		text_panel.add(age_txt);
 		text_panel.add(phone_txt);
 		text_panel.add(Adress_txt);
+		text_panel.add(btn_save);
 		
+		
+		btn_empty.setBounds(870, 600, 300, 100);
 		btn_backMain.setBounds(870,720,300,100);
 		name.setBounds(50, 50, 100, 100);
 		age.setBounds(50, 200, 100, 100);
@@ -53,7 +58,7 @@ public class Add_Staff_Frame extends JFrame{
 		age_txt.setBounds(120, 240, 200, 30);
 		phone_txt.setBounds(210, 390,270 , 30);
 		Adress_txt.setBounds(120, 540, 300, 100);
-			
+		btn_save.setBounds(100,680 , 300, 100);	
 		
 		
 		btn_backMain.addActionListener(new ActionListener() {
@@ -65,6 +70,16 @@ public class Add_Staff_Frame extends JFrame{
 			}
 		});
 		
+		btn_empty.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				name_txt.setText(" ");
+				age_txt.setText(" ");
+				phone_txt.setText(" ");
+				Adress_txt.setText(" ");
+			}
+		});
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setSize(1200,900);
