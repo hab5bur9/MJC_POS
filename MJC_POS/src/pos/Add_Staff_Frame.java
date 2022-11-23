d00c68630b19e144aa10f00578bb0362a58b907c
ca080 <ca080@221.146.213.228>
2022-11-23 18:13:19 +0900

    .

--- MJC_POS/src/pos/Add_Staff_Frame.java
+++ MJC_POS/src/pos/Add_Staff_Frame.java
@@ -9,7 +9,7 @@ public class Add_Staff_Frame extends JFrame{
 	JPanel text_panel=new JPanel();
 	
 	
-	JLabel ec=new JLabel(new ImageIcon("img/anonymous2.png"));
+	//JLabel ec=new JLabel(new ImageIcon("img/anonymous2.png"));
 	JLabel name = new JLabel("이름(name):");
 	JLabel age= new JLabel("나이(age):");
 	JLabel phone_number=new JLabel("핸드폰 번호(phone number):");
@@ -33,7 +33,7 @@ public class Add_Staff_Frame extends JFrame{
 		add(main_panel);
 		main_panel.setLayout(null);
 		text_panel.setLayout(null);
-		text_panel.setBounds(350, 25, 500, 800);
+		text_panel.setBounds(0 , 0, 500, 800);
 		text_panel.setBackground(Color.LIGHT_GRAY);
 		main_panel.add(text_panel);
 		main_panel.add(btn_backMain);
@@ -51,18 +51,18 @@ public class Add_Staff_Frame extends JFrame{
 		text_panel.add(btn_save);
 		
 		
-		ec.setBounds(870, 27, 300, 300);
+		//ec.setBounds(870, 27, 300, 300);
 		btn_empty.setBounds(870, 600, 300, 100);
 		btn_backMain.setBounds(870,720,300,100);
-		name.setBounds(50, 50, 100, 100);
-		age.setBounds(50, 200, 100, 100);
-		phone_number.setBounds(50, 350, 300, 100);
-		Adress.setBounds(50, 500, 100, 100);
+		name.setBounds(15, 50, 100, 100);
+		age.setBounds(15, 200, 100, 100);
+		phone_number.setBounds(15, 350, 300, 100);
+		Adress.setBounds(15, 500, 100, 100);
 		
 		
 		name_txt.setBounds(130, 90, 200, 30);
-		age_txt.setBounds(120, 240, 200, 30);
-		phone_txt.setBounds(210, 390,270 , 30);
+		age_txt.setBounds(130, 240, 200, 30);
+		phone_txt.setBounds(190, 390,270 , 30);
 		Adress_txt.setBounds(120, 540, 300, 100);
 		btn_save.setBounds(150,680 , 200, 100);	
 		
@@ -89,7 +89,7 @@ public class Add_Staff_Frame extends JFrame{
 		});
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		setSize(1200,900);
+		setSize(500,800);
 		setPreferredSize(new Dimension(1200,900));
 	}
 	
