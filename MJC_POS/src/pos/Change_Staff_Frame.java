bcbb8bf66aab5137c1226c27ae8a435d2e2f4f29
hsmik <hsmik@DESKTOP-KDU9G4I>
2022-11-26 18:49:39 +0900

    kb

--- MJC_POS/src/pos/Change_Staff_Frame.java
+++ MJC_POS/src/pos/Change_Staff_Frame.java
@@ -4,11 +4,12 @@ import java.awt.event.*;
 import java.util.Calendar;
 import java.awt.*;
 
-public class Frame_SalesInquiry extends JFrame {
-	Frame_SalesInquiry(){
+public class Change_Staff_Frame extends JFrame{
+	
+	Change_Staff_Frame(){
 		//프레임 기본 설정
-		setTitle("매출조회");
-		setSize(1200, 900);
+		setTitle("정보 수정");
+		setSize(500, 800);
 		setVisible(true);
 		setLocationRelativeTo(null);// 화면 가운데서 창이 나옴
 		setResizable(false);//정해진사이즈에서 변경불가
@@ -19,10 +20,7 @@ public class Frame_SalesInquiry extends JFrame {
 		
 		
 		
-		
 	}
 	
-	public static void main(String[] args) {
-		new Frame_SalesInquiry();
-	}
+	
 }
