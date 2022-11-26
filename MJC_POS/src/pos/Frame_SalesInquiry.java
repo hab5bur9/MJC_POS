453c5ed6e30664afcae1f87a13e4d9c9b3ddc163
hsmik <hsmik@DESKTOP-KDU9G4I>
2022-11-26 18:42:41 +0900

    kb

--- MJC_POS/src/pos/Frame_SalesInquiry.java
+++ MJC_POS/src/pos/Frame_SalesInquiry.java
@@ -1,5 +1,28 @@
 package pos;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Calendar;
+import java.awt.*;
 
-public class Frame_SalesInquiry {
-
+public class Frame_SalesInquiry extends JFrame {
+	Frame_SalesInquiry(){
+		//프레임 기본 설정
+		setTitle("매출조회");
+		setSize(500, 800);
+		setVisible(true);
+		setLocationRelativeTo(null);// 화면 가운데서 창이 나옴
+		setResizable(false);//정해진사이즈에서 변경불가
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 안정적으로 JFrame이 종료되게 해줌
+				
+		Container ct = getContentPane();
+		ct.setLayout(null); //setBounds 사용하기 위해 null
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new Frame_SalesInquiry();
+	}
 }
